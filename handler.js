ce3a7b9038438350ff1e0568dbed3a3aa2fa8ceb
Caleb Keller <caleb@calebjkeller.com>
2021-10-31 18:29:47 -0400

    Set up api and function to read xlsx

--- handler.js
+++ handler.js
@@ -1,15 +1,19 @@
 'use strict';
+const Excel = require('exceljs');
 
 module.exports.geocode = async (event) => {
+
+  const workbook = new Excel.Workbook();
+  await workbook.xlsx.load(Buffer.from(event.body, 'base64'));
+
+  var str = "";
+
+  workbook.worksheets[0].eachRow(function(row, rowNumber) {
+    str += JSON.stringify(row.values) + ", ";
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v2.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: str
   };
 };

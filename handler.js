99a10a24816bd35942dee3eed7f1e48c40b7c17a
Caleb Keller <caleb@calebjkeller.com>
2021-11-05 13:08:21 -0400

    Created file reading functionality

--- handler.js
+++ handler.js
@@ -3,17 +3,41 @@ const Excel = require('exceljs');
 
 module.exports.geocode = async (event) => {
 
-  const workbook = new Excel.Workbook();
-  await workbook.xlsx.load(Buffer.from(event.body, 'base64'));
+  var reader;
+  switch(event.headers["content-type"]) {
+    case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': {
+      reader = new Excel.Workbook();
+      await reader.xlsx.load(Buffer.from(event.body, 'base64'));
+    }
+    break;
 
-  var str = "";
+    case "text/csv": {
+      reader = new Excel.Workbook();
+      await reader.csv.read(event.body);
+    }
+    break;
 
-  workbook.worksheets[0].eachRow(function(row, rowNumber) {
-    str += JSON.stringify(row.values) + ", ";
-  });
+    default: {
+      return {
+        statusCode: 500,
+        body: "Unable to read file"
+      }
+    }
+  }
+
+  reader.worksheets[0].columns = [
+    {header: 'address', key: 'Address'},
+    {header: "name", key: "Name"},
+    {header: "request", key: "Request"}
+  ];
+
+  var addresses = reader.worksheets[0].getColumn("Address").values;
+  var requirement = reader.worksheets[0].getColumn("Request").values;
+  var meta = [];
 
   return {
     statusCode: 200,
-    body: str
-  };
-};
+    body: JSON.stringify({"addresses" : addresses, "req" : requirement})
+  }
+  
+}
